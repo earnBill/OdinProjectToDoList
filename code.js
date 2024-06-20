57413ac85a3fa7993262be7093f29177ef0d3ab2
BillLaptop <earnappbill@gmail.com>
2024-06-20 13:12:34 +0300

    Create newTAsk function

--- code.js
+++ code.js
@@ -1,7 +1,15 @@
+let defaultProject = [];
+let addTask = document.querySelector('.add-task');
+let addProject = document.querySelector('.add-project');
+
+addTask.addEventListener('click', () => {
+    defaultProject.push(createTask('bill', 'bill', '20', 'bill'));
+    console.log(defaultProject)
+})
+
 function createTask(title, description, dueDate, priority) {
     return { title, description, dueDate, priority };
 }
 
-let bill = createTask('Bnb','Go to work', '10', 'hight');
-
-console.log(bill);
+defaultProject.push(createTask('Bnb','Go to work', '10', 'hight'));
+console.log(defaultProject);
